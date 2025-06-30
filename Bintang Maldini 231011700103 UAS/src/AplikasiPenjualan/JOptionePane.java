a3ca5199c2f2f71c5f6b21bee1f3a5e5487f10da
connyhoper <mhmmdbld24@gmail.com>
2025-06-30 14:06:15 +0700

    Add files via upload

---
+++ Bintang Maldini 231011700103 UAS/src/AplikasiPenjualan/JOptionePane.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AplikasiPenjualan;
+
+/**
+ *
+ * @author X13 Yoga
+ */
+class JOptionePane {
+
+    static int OK_CANCEL_OPTION;
+
+    static int showConfirmDialog(Object object, String keluar_aplikasi, String konfirmasi, int OK_CANCEL_OPTION) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
